ddbac036fc2e4e87ac8ce9dabd910debfdd706dd
Steve Losh <steve@stevelosh.com>
2019-12-20 17:12:29 -0500

    SSET

---
+++ src/problems/sset.lisp
@@ -0,0 +1,16 @@
+(in-package :rosalind)
+
+;; The cardinality of a power set is 2ⁿ, because you can represent an individual
+;; set as a binary string where 1 means the element is included and 0 is not,
+;; and there are 2ⁿ possible binary strings of length n.
+
+(define-problem sset (data stream)
+    "3"
+    "8"
+  (mod (expt 2 (read data)) 1000000))
+
+
+#; Scratch --------------------------------------------------------------------
+
+(problem-sset)
+(solve sset)
